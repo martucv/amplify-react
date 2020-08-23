49b1f88ae027aea665dd73ebf88410c5a382ed2a
Valentina Martucci <martucv@amazon.com>
2020-08-23 18:32:05 +0100

    react with auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator } from '@aws-amplify/ui-react'
 
 function App() {
   return (
@@ -23,4 +24,5 @@ function App() {
   );
 }
 
-export default App;
+//export default App;
+export default withAuthenticator(App);
